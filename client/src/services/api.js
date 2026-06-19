f31535c0f98adc82f735c3611cd606dbc4ce836d
Muqqeeth <muqqeeth@gmail.com>
2026-06-19 20:41:25 +0530

    add api url debug

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-console.log(import.meta.env.VITE_API_URL);
+console.log("API URL:", process.env.REACT_APP_API_URL);
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL + '/api',
   headers: { 'Content-Type': 'application/json' },
